f80d185d3bf0ca53e1d4c2ca3225e08591825bce
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2024-12-30 17:14:35 -0600

    Stage 1 styling on Section 2 (Experience)

--- components/projectCard.js
+++ components/projectCard.js
@@ -5,6 +5,7 @@ class ProjectCard extends HTMLElement {
         const previewLink = this.getAttribute('preview');
         const name = this.getAttribute('name');
         const affiliation = this.getAttribute('org');
+        //categories, like frontend, but also hackathons vs personal (can use org affiliation)
         const description = this.getAttribute('description');
         const projectLink = this.getAttribute('link');
         const githubLink = this.getAttribute('github');
@@ -20,10 +21,13 @@ class ProjectCard extends HTMLElement {
         <div id="project-card">
             <div id="project-preview">${previewLink}</div>
             <h3 id="project-title">${name}</h3>
-            <div id=project-affiliation">${affiliation}</div>
-            <p id="project-description">${description}</p>
-            <div id="project-links">${projectLink} & ${githubLink}</div>
+            <div id="project-affiliation">${affiliation}</div>
             <div id="project-tags">${skillTags}</div>
+            <p id="project-description">${description}</p>
+            <ul class="flex-container-row" id="project-links">
+                ${projectLink != '' ? `<a href={projectLink} target="_blank" rel="noopener"><i class="material-icons">link</i></a>` : ``}
+                ${githubLink != '' ? `<a href=${githubLink} target="_blank" rel="noopener"><img class="icon-logo" src="assets/brand-icons/github-logo.svg" alt="Link to my Github"></a>` : ``}
+            </ul>
         </div>
       `;
     }
