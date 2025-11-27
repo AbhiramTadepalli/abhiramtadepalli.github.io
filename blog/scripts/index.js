303d77b0f9692ab12e15455d139bac4ee118abd4
AbhiramTadepalli <abhiram.tadepalli.7@gmail.com>
2025-11-27 10:13:39 -0600

    mobile hamburger menu in blog page

--- blog/scripts/index.js
+++ blog/scripts/index.js
@@ -33,4 +33,17 @@ function updateSidebarPosition(isBlog = false) {
   window.addEventListener('scroll', () => updateSidebarPosition(true));
   window.addEventListener('resize', () => updateSidebarPosition(true));
   updateSidebarPosition(true); // first time
+
+  /** Mobile Navbar */
+  const hamburger = document.getElementById('mobile-nav-button');
+  const navMenu = document.getElementById('mobile-nav-menu');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navMenu.classList.toggle('active');
+      
+      // Update aria-expanded for accessibility
+      const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
+      hamburger.setAttribute('aria-expanded', !isExpanded);
+  });
 });
